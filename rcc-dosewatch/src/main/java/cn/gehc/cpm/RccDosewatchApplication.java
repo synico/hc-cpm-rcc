2036ac0115a34ce5f99bc44c33bd9f1900d52a28
Nick Liu <synico@qq.com>
2019-09-20 08:06:20 +0800

    udpate view v_exam

--- rcc-dosewatch/src/main/java/cn/gehc/cpm/RccDosewatchApplication.java
+++ rcc-dosewatch/src/main/java/cn/gehc/cpm/RccDosewatchApplication.java
@@ -2,7 +2,6 @@ package cn.gehc.cpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
---
+++ rcc-dosewatch/src/main/resources/sql/index.sql
@@ -0,0 +1,7 @@
+create index his_pre_body_part_idx on his_exam(pre_exam_body_part);
+
+create index ris_body_part_idx on ris_exam(exam_body_part);
+
+create index his_sheetid_idx on his_exam(sheetid);
+
+create index ris_sheetid_idx on ris_exam(sheetid);
